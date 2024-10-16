f3fe90a337544d348c6949848c5465201e0d7bc7
lothairegoubert <lothairegoubert@gmail.com>
2024-10-16 17:28:43 +0200

    test fx

--- Code/Main.java
+++ Code/Main.java
@@ -6,30 +6,9 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-        // Créer un bouton
-        Button btn = new Button();
-        btn.setText("Cliquez-moi !");
-        btn.setOnAction(event -> System.out.println("Bonjour, JavaFX !"));
-
-        // Ajouter le bouton dans un layout
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        // Créer la scène
-        Scene scene = new Scene(root, 300, 250);
-
-        // Configurer la fenêtre (Stage)
-        primaryStage.setTitle("Mon Application JavaFX");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+public class Main {
 
     public static void main(String[] args) {
-        // Lancer l'application JavaFX
-        launch(args);
+        System.out.println("Hello World");
     }
 }
