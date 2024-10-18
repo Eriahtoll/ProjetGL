75191c9b3f01e2d092ac051fbbf661fa6451a1e6
lothairegoubert <lothairegoubert@gmail.com>
2024-10-18 14:30:28 +0200

    mise des méthodes

--- Code/Model/CalculatorModel.java
+++ Code/Model/CalculatorModel.java
@@ -7,5 +7,53 @@ public class CalculatorModel implements CalculatorModelInterface {
     public CalculatorModel() {
     }
 
+    @Override
+    public void add() {
 
+    }
+
+    @Override
+    public void substract() {
+
+    }
+
+    @Override
+    public void multiply() {
+
+    }
+
+    @Override
+    public void divide() {
+
+    }
+
+    @Override
+    public void opposite() {
+
+    }
+
+    @Override
+    public void push() {
+
+    }
+
+    @Override
+    public void pop() {
+
+    }
+
+    @Override
+    public void drop() {
+
+    }
+
+    @Override
+    public void swap() {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
 }
