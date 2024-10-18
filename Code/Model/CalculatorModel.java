e04db99e3fd5fe680e3f636a96314e3372c4ce6b
Arthur71130 <ademacedo.71@gmail.com>
2024-10-18 14:53:21 +0200

    fonctions

--- Code/Model/CalculatorModel.java
+++ Code/Model/CalculatorModel.java
@@ -3,10 +3,9 @@ package Code.Model;
 import java.util.Stack;
 
 public class CalculatorModel implements CalculatorModelInterface {
-    private Stack Pile = new Stack();
+    private Stack<Double> Pile = new Stack();
     public CalculatorModel() {
     }
-
     @Override
     public void add() {
         if(Pile.size()>1){
@@ -28,12 +27,34 @@ public class CalculatorModel implements CalculatorModelInterface {
 
     @Override
     public void multiply() {
+        if (Pile.size() >= 2) {
+            double value1 = Pile.pop();
+            double value2 = Pile.pop();
 
+            double result = value1 * value2;
+
+            Pile.push(result);
+        } else {
+            System.out.println("La pile contient moins de deux éléments");
+        }
     }
-
     @Override
     public void divide() {
+        if (Pile.size() >= 2) {
+            Double value1 = Pile.pop();
+            Double value2 = Pile.pop();
 
+            if (value1 != 0) {
+                Double result = value2 / value1;
+                Pile.push(result);
+            } else {
+                Pile.push(value2);
+                Pile.push(value1);
+                System.out.println("Impossible de diviser par zéro.");
+            }
+        } else {
+            System.out.println("La pile contient moins de deux éléments");
+        }
     }
 
     @Override
@@ -56,12 +77,22 @@ public class CalculatorModel implements CalculatorModelInterface {
 
     @Override
     public void drop() {
-
+        if (Pile.size() >= 1) {
+            Pile.pop();
+        } else {
+            System.out.println("La pile est vide");
     }
 
     @Override
     public void swap() {
-
+        if (Pile.size() >= 2) {
+            double value1 = Pile.pop();
+            double value2 = Pile.pop();
+            Pile.push(value1);
+            Pile.push(value2);
+        } else {
+            System.out.println("La pile contient moins de deux éléments");
+        }
     }
 
     @Override
