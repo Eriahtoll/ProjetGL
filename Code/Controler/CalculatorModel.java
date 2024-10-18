902fdbc2710bd87e43e544dba5ff446c1d839e8e
lothairegoubert <lothairegoubert@gmail.com>
2024-10-18 14:19:57 +0200

    avancement sur calculator

--- Code/Controler/CalculatorModel.java
+++ Code/Controler/CalculatorModel.java
@@ -1,5 +1,12 @@
 package Code.Controler;
 
-public class CalculatorModel {
+import Code.Model.CalculatorModelInterface;
+
+import java.util.Stack;
+
+public class CalculatorModel implements CalculatorModelInterface {
+    private Stack Pile = new Stack();
+    public CalculatorModel() {
+    }
 
 }
