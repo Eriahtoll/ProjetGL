06354c85483e89cfa9589722601388cd830c1601
Arthur71130 <ademacedo.71@gmail.com>
2024-10-18 14:17:14 +0200

    Ajout des fonctions dans CalculatorControlerInterface.java

--- Code/Controler/CalculatorControlerInterface.java
+++ Code/Controler/CalculatorControlerInterface.java
@@ -1,4 +1,14 @@
 package Code.Controler;
 
 public interface CalculatorControlerInterface {
+    public double add();
+    public double substract();
+    public double multiply();
+    public double divide();
+    public double opposite();
+    public void push();
+    public void pop();
+    public void drop();
+    public void swap();
+    public void clear();
 }
--- Code/Controler/CalculatorModel.java
+++ Code/Controler/CalculatorModel.java
@@ -2,4 +2,5 @@ package Code.Controler;
 
 public class CalculatorModel {
 
+
 }
