489c2313d4444027b67a5941313ec60145e6443f
lothairegoubert <lothairegoubert@gmail.com>
2024-10-25 16:22:48 +0200

    regu

--- Code/View/Buttons.java
+++ Code/View/Buttons.java
@@ -9,7 +9,6 @@ public class Buttons extends javafx.scene.control.Button {
         this.setStyle(
                 "-fx-background-color: black; " +
                         "-fx-text-fill: white; " +
-                        "-fx-font-size: 20px; " +
                         "-fx-font-weight: bold;"
         );
         this.setPrefSize(100,100);
--- Code/View/CalculatorGUI.java
+++ Code/View/CalculatorGUI.java
@@ -8,6 +8,7 @@ import javafx.scene.input.Dragboard;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.Mnemonic;
 import javafx.scene.input.TransferMode;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
 import javafx.util.Callback;
@@ -21,8 +22,27 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class CalculatorGUI implements CalculatorGUIInterface {
+    Buttons button1 = new Buttons("1");
+    Buttons button2 = new Buttons("2");
+    Buttons button3 = new Buttons("3");
+    Buttons button4 = new Buttons("4");
+    Buttons button5 = new Buttons("5");
+    Buttons button6 = new Buttons("6");
+    Buttons button7 = new Buttons("7");
+    Buttons button8 = new Buttons("8");
+    Buttons button9 = new Buttons("9");
+    Buttons button0 = new Buttons("0");
+    Buttons buttonPLUS = new Buttons("+");
+    Buttons buttonMOINS = new Buttons("-");
+    Buttons buttonFOIS = new Buttons("*");
+    Buttons buttonDIVIS = new Buttons("/");
+    Buttons buttonENT = new Buttons("<>");
+    Buttons buttonDEL = new Buttons("DEL");
+    Buttons buttonCLEAR = new Buttons("AC");
+    Buttons buttonVIRG = new Buttons(",");
+    Buttons buttonSWAP = new Buttons("<=>");
 
-
+    GridPane tom = new GridPane();
 
 
 
